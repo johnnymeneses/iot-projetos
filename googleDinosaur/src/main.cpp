5e39883b7587385d4e55771210d06cb2523d51ce
johnnymeneses <bilbo_vader@outlook.com>
2021-09-15 18:02:16 -0300

    Projeto Dinossauro Google

---
+++ googleDinosaur/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo servo_2;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+
+  servo_2.attach(2);
+
+}
+
+
+void acionaServo()
+{
+  Serial.println(analogRead(A0));
+  if (analogRead(A0) < 300) {
+    servo_2.write(60);
+    delay(150); // Wait for 150 millisecond(s)
+  } else {
+    servo_2.write(0);
+  }
+
+}
+
+void loop()
+{
+  acionaServo();
+}
+
+
